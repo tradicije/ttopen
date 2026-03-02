97558b64911ac5781f2f504232a6043561a3f61f
Aleksa Dimitrijević <tradicije@icloud.com>
2026-03-02 17:26:39 +0100

    Changed author name and added author uri

--- stkb-unified-core.php
+++ stkb-unified-core.php
@@ -3,7 +3,8 @@
  * Plugin Name: OpenTT
  * Description: OpenTT sistem za vođenje i prikaz stonoteniskih takmičenja, klubova i igrača: admin meni, DB tabele i kompatibilno učitavanje postojećih shortcode modula.
  * Version: 1.0.0
- * Author: OpenTT
+ * Author: Aleksa Dimitrijević
+ * Author URI: https://instagram.com/tradicije
  * License: AGPL-3.0-or-later
  * License URI: https://www.gnu.org/licenses/agpl-3.0.html
  */
